df747598839a331b29db7bdbdd781f15544d8578
lbartell <lenabartell@gmail.com>
2016-12-03 21:10:46 -0500

    Add functions to count, display, and save song counts

--- example.py
+++ example.py
@@ -2,5 +2,36 @@
 Example code to test xpn_wordplay
 '''
 
+from wordplay import *
 
+# get list of song titles & artists
+songlist = get_songs()
 
+# print songlist
+num_songs = len(songlist)
+if num_songs <=10:
+    for song in songlist:
+        print '%s - %s'%(song)
+else:
+    for song in songlist[:5]:
+        print '%s - %s'%(song)
+    print '...'
+    for song in songlist[-5:]:
+        print '%s - %s'%(song)
+print 'Total number of songs: %d'%num_songs
+
+# get song title occurances, title word occurances, and artist occurances
+titles = [song[0] for song in songlist]
+artists = [song[1] for song in songlist]
+title_word_counts = count_words(titles, break_words=True)
+title_counts = count_words(titles, break_words=False)
+artist_counts = count_words(artists, break_words=False)
+
+# print title, title-word and artists top-ten lists
+print_top(title_counts, title='titles')
+print_top(title_word_counts, title='title words')
+print_top(artist_counts, title='artists')
+
+save_counts(title_counts, filename='title_counts.txt')
+save_counts(title_word_counts, filename='title_word_counts.txt')
+save_counts(artist_counts, filename='artist_counts')
--- wordplay.py
+++ wordplay.py
@@ -1,19 +1,20 @@
 '''
-Dowload and display stats about WXPN's A-Z songs playlist
+Download and display stats about WXPN's A-Z songs playlist
 '''
 
 # imports
 import urllib
 import urlparse
 import re
+import string
 
 # function definitions
-def get_songs(url='http://origin.xpn.org/static/az.php', letter='a',
-                  pattern=r'<li>([^<]+) - ([^<]+)</li>'):
+def url_songs(url='http://origin.xpn.org/static/az.php', letter='a',
+                  pattern=r'<li>([^<]+) - ([^<]+)</li>', remove_duplicates=True):
     '''
     Download data from the provided url, querying for all songs that start with
-    the given letter and return a list of songs. Url text is parsed usign the
-    regular expression pattern.
+    the given letter and return a list of songs. Url text is parsed using the
+    regular expression given in pattern.
     '''
 
     # construct the url, adding a query for the given letter
@@ -37,6 +38,113 @@ def get_songs(url='http://origin.xpn.org/static/az.php', letter='a',
     pattern = r'<li>([^<]+) - ([^<]+)</li>'
     songs = re.findall(pattern, url_text)
 
+    # remove duplicate listings
+    if remove_duplicates:
+        unique_songs = []
+        for song in songs:
+            if song not in unique_songs:
+                unique_songs.append(song)
+        songs = unique_songs
+
     return songs
+
+
+def get_songs():
+    '''
+    Download and return list of all songs a-z
+    '''
+
+    # loop over each letter to construct full songlist
+    letters = tuple(string.ascii_lowercase)
+    songlist = []
+    print 'Downloading',
+    for letter in letters:
+        print '%s'%(letter),
+
+        songs = url_songs(letter=letter)
+        if songs:
+            songlist = songlist + songs
+
+    print
+    return songlist
+
+
+def count_words(stringlist, break_words=True):
+    '''
+    Given a list of strings, return a list of tuples containing each unique
+    word or phrase and its number of occurances (ignoring case).
+    If break_words is true, then strings are broken into individual words which
+    are then compared.
+    If break_words if false, strings are not broken into words, and the input
+    strings/phrases are compared.
+    '''
+
+    # combine all strings and split into a list of words, if requested
+    if break_words:
+        word_list = re.findall(r"[\w']+", ' '.join(stringlist).lower())
+    else:
+        word_list = stringlist
+    word_dict = {}
+
+    # loop over each word/pharase and either add to the dictionary or increment
+    for word in word_list:
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+
+    # list of tuples with each word and its number of occurances, sorted by
+    # decreasing number of occurances
+    word_counts = zip(word_dict.keys(), word_dict.values())
+    word_counts = sorted(word_counts, key=lambda x: x[1], reverse=True)
+
+    return word_counts
+
+def print_top(word_counts, title='', num=10):
+    '''
+    given a list of string-count tuples, print the first ten entries
+    '''
+
+    print '========================='
+    print 'Top %d %s:'%(num, title)
+    print '========================='
+
+    ix = 1
+
+    if len(word_counts) > num:
+        for (word, count) in word_counts[:num]:
+            print '#%d: %03d %s'%(ix, count, word)
+            ix += 1
+    else:
+        for (word, count) in word_counts:
+            print '#%d: %03d %s'%(ix, count, word)
+            ix += 1
+
+
+def save_counts(word_counts, filename='word_counts.txt'):
+    '''
+    Save list of string-count tuples in a text file with the name filename
+    '''
+    f = open(filename, 'w')
+    data = ['%03d %s'%(b,a) for (a,b) in word_counts]
+    f.write('\n'.join(data))
+    f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
