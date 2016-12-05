b139efabb17dd98e28e1f16e4ddee060ef872344
lbartell <lenabartell@gmail.com>
2016-12-04 22:21:33 -0500

    Add function to get release year and update data

--- example.py
+++ example.py
@@ -1,29 +1,74 @@
 '''
-Example code to run xpn_wordplay
+Code to run xpn_wordplay to download, save, and analyze XPN A-Z playlist data
+
+Author: Lena Bartell, github.com/lbartell
 '''
 
 from wordplay import *
 import datetime
+import string
+import os
+import csv
 
-# get list of song titles & artists
-songlist = get_songs()
+## ====================
+## Gather all song data
+## ====================
+letters = tuple('abcdefg') #letters = tuple(string.ascii_lowercase)
+full_songlist = []
+
+# gather data from all letters
+for letter in letters:
+    datafilename = letter+'_data.txt'
+
+    # see if the data file already exists
+    if os.path.isfile(datafilename):
+        # if yes, import it
+
+        with open(datafilename, 'r') as f:
+            reader = csv.reader(f, delimiter='\t')
+            for row in reader:
+                row[0] = int(row[0])
+                full_songlist.append(tuple((row[1], row[2], row[0])))
+
+    else:
+        # otherwise, gather and save data from the associated letter
+
+        # get song titles and artists and release year
+        songlist = get_songs(letters=letter)
+        years = get_release_years(songlist=songlist)
+        titles = [x[0] for x in songlist]
+        artists = [x[1] for x in songlist]
+        songlist = zip(titles, artists, years)
+
+        # save the data
+        save_song_info(infolist=songlist, filename=datafilename)
+
+        # add data for full list
+        full_songlist += songlist
+
+## ====================================
+## Analyze all song data & save results
+## ====================================
+
+all_titles = [x[0] for x in full_songlist]
+all_artists = [x[1] for x in full_songlist]
+all_years = [x[2] for x in full_songlist]
+
+# calculate and save title, title-word and artists top-ten lists
+title_word_counts = count_list(all_titles, break_words=True)
+title_counts = count_list(all_titles, break_words=False)
+artist_counts = count_list(all_artists, break_words=False)
+year_counts = count_list(all_years, break_words=False)
+
+top_words = print_top(title_word_counts, title='title words', num=50)
+top_titles = print_top(title_counts, title='titles', num=50)
+top_artists = print_top(artist_counts, title='artists', num=50)
+top_years = print_top(year_counts, title='release years', num=50)
+
+# save top lists to a text file
 now = datetime.datetime.now()
 nowstr = 'Last updated: %04d-%02d-%02d, %02d:%02d'%(now.year, now.month, now.day,
                                                    now.hour, now.minute)
-
-# get song title occurances, title word occurances, and artist occurances
-titles = [song[0] for song in songlist]
-artists = [song[1] for song in songlist]
-title_word_counts = count_words(titles, break_words=True)
-title_counts = count_words(titles, break_words=False)
-artist_counts = count_words(artists, break_words=False)
-
-# print title, title-word and artists top-ten lists
-top_titles = print_top(title_counts, title='titles', num=50)
-top_words = print_top(title_word_counts, title='title words', num=50)
-top_artists = print_top(artist_counts, title='artists', num=50)
-
-# save top lists to a text file
 f = open('top_50_lists.txt', 'w')
 f.write('WXPN A to Z analysis #XPNAtoZ www.xpn.org, by Lena Bartell\n')
 f.write(nowstr)
@@ -33,9 +78,23 @@ f.write('\n')
 f.write(top_titles)
 f.write('\n')
 f.write(top_words)
+f.write('\n')
+f.write(top_years)
 f.close()
 
 # save title, title-word and artists counts
 save_counts(title_counts, filename='top_titles.txt')
 save_counts(title_word_counts, filename='top_title_words.txt')
 save_counts(artist_counts, filename='top_artists.txt')
+save_counts(year_counts, filename='top_artists.txt')
+
+
+
+
+
+
+
+
+
+
+
--- wordplay.py
+++ wordplay.py
@@ -1,5 +1,7 @@
 '''
-Download and display stats about WXPN's A-Z songs playlist
+Functions to download and display stats about WXPN's A-Z songs playlist
+
+Author: Lena Bartell, github.com/lbartell
 '''
 
 # imports
@@ -7,21 +9,22 @@ import urllib
 import urlparse
 import re
 import string
+import discogs_client
+import warnings
 
 # function definitions
-def url_songs(url='http://origin.xpn.org/static/az.php', letter='a',
+def parse_url(url='http://origin.xpn.org/static/az.php', query='a',
                   pattern=r'<li>([^<]+) - ([^<]+)</li>', remove_duplicates=True):
     '''
-    Download data from the provided url, querying for all songs that start with
-    the given letter and return a list of songs. Url text is parsed using the
-    regular expression given in pattern.
+    Download data from the provided url, adding the given query and return a
+    list of the results matching the regular expression given in pattern.
     '''
 
     # construct the url, adding a query for the given letter
     url_parts = list(urlparse.urlparse(url))
-    query = dict(urlparse.parse_qsl(url_parts[4]))
-    query.update({'q':letter})
-    url_parts[4] = urllib.urlencode(query)
+    queryobj = dict(urlparse.parse_qsl(url_parts[4]))
+    queryobj.update({'q':query})
+    url_parts[4] = urllib.urlencode(queryobj)
     full_url = urlparse.urlunparse(url_parts)
 
     # Read the url
@@ -36,32 +39,35 @@ def url_songs(url='http://origin.xpn.org/static/az.php', letter='a',
 
     # Parse the url text into a list of tuples (title, artist)
     pattern = r'<li>([^<]+) - ([^<]+)</li>'
-    songs = re.findall(pattern, url_text)
+    results = re.findall(pattern, url_text)
 
     # remove duplicate listings
     if remove_duplicates:
-        unique_songs = []
-        for song in songs:
-            if song not in unique_songs:
-                unique_songs.append(song)
-        songs = unique_songs
+        unique_results = []
+        for item in results:
+            if item not in unique_results:
+                unique_results.append(item)
+        results = unique_results
 
-    return songs
+    return results
 
 
-def get_songs():
+def get_songs(letters=''):
     '''
-    Download and return list of all songs a-z
+    Download and return list of all songs starting with the letters listed in
+    the input letters. Default is to use all letters a-z
     '''
 
     # loop over each letter to construct full songlist
-    letters = tuple(string.ascii_lowercase)
+    if not letters:
+        letters = tuple(string.ascii_lowercase)
+
     songlist = []
     print 'Downloading',
     for letter in letters:
         print '%s'%(letter),
 
-        songs = url_songs(letter=letter)
+        songs = parse_url(query=letter)
         if songs:
             songlist = songlist + songs
 
@@ -69,7 +75,7 @@ def get_songs():
     return songlist
 
 
-def count_words(stringlist, break_words=True):
+def count_list(stringlist, break_words=True):
     '''
     Given a list of strings, return a list of tuples containing each unique
     word or phrase and its number of occurances (ignoring case).
@@ -96,7 +102,7 @@ def count_words(stringlist, break_words=True):
     # list of tuples with each word and its number of occurances, sorted by
     # decreasing number of occurances
     word_counts = zip(word_dict.keys(), word_dict.values())
-    word_counts = sorted(word_counts, key=lambda x: x[1], reverse=True)
+    word_counts = sorted(word_counts, key=lambda x:(x[1], x[0]), reverse=True)
 
     return word_counts
 
@@ -104,25 +110,25 @@ def print_top(word_counts, title='', num=10):
     '''
     given a list of string-count tuples, print the first ten entries
     '''
-    string = []
+    stringlist = []
     ix = 1;
 
-    string.append('==============================')
-    string.append('Top %d %s:'%(num, title))
-    string.append('------------------------------')
-    string.append('#Rank: Occurances Word/Phrase')
-    string.append('==============================')
+    stringlist.append('==============================')
+    stringlist.append('Top %d %s:'%(num, title))
+    stringlist.append('------------------------------')
+    stringlist.append('#Rank: Occurances Item')
+    stringlist.append('==============================')
 
     if len(word_counts) > num:
         for (word, count) in word_counts[:num]:
-            string.append('#%04s: %10s %s'%(str(ix), str(count), word))
+            stringlist.append('#%04s: %10s %s'%(str(ix), str(count), str(word)))
             ix += 1
     else:
         for (word, count) in word_counts:
-            string.append('#%04s: %10s %s'%(str(ix), str(count), word))
+            stringlist.append('#%04s: %10s %s'%(str(ix), str(count), str(word)))
             ix += 1
 
-    fullstring = '\n'.join(string) +'\n'
+    fullstring = '\n'.join(stringlist) +'\n'
     print fullstring
 
     return fullstring
@@ -136,16 +142,66 @@ def save_counts(word_counts, filename='word_counts.txt'):
     f.write('\n'.join(data))
     f.close()
 
+def get_release_years(songlist=[], token='', search_type='release', verbose=True):
+    '''
+    Use Discogs API to get the release year for each (title, artist) tuple
+    in the list songlist and return as a list of integer years.
 
+    Notes:
+    If the search is unsuccessful, the year is set to be 0.
+    The year of the top result is result is returned.
+    Discogs API is authenticated using token.
+    '''
+    if not verbose:
+        print 'Working...',
 
+    # read user token if not given
+    if not token:
+        f = open('discogsapikey')
+        token = f.readline()
+        f.close()
 
+    # setup the api client
+    d = discogs_client.Client('XPN_wordplay/0.1', user_token=token)
+    years = []
 
+    # loop over each song title/artist tuple
+    for tup in songlist:
+        searchstr = ' - '.join(tup)
+        try:
+            results = d.search(searchstr, type='release')[0]
+            if verbose:
+                print 'ARTIST: %s'%results.artists[0].name,
+                print 'ALBUM: %s'%results.title,
+                print 'YEAR: %s'%results.year
+            years.append(int(results.year))
 
+        except:
+            # if it doesn't work, print a message and return 0 as the year
+            warnings.warn('Problem finding year for %s, setting as 0'%searchstr)
+            years.append(0)
 
+    if not verbose:
+        print 'Done'
+    return years
 
+def save_song_info(infolist=[], headerstr=None, pattern='{2:d}\t{0}\t{1}\n',
+                   filename='output.txt'):
+    '''
+    Save the list of info in infolist to the file given by filename using
+    pattern to format each item in the list into a string. If headerstr is
+    provided, this is inserted at the start of the file
+    '''
 
+    f = open(filename, 'w')
+    if headerstr:
+        f.write(headerstr+'\n')
 
+    for item in infolist:
+        f.write(pattern.format(*item))
 
+    f.close()
+    return
 
 
 
